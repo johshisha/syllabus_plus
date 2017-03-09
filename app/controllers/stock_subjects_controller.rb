ba22f9f5ede5949c23a6635b83da4e5745bb2b79
johshisha <johshisha@gmail.com>
2017-03-10 01:59:30 +0900

    Stock and unstock subjects

--- app/controllers/stock_subjects_controller.rb
+++ app/controllers/stock_subjects_controller.rb
@@ -1,7 +1,7 @@
 class StockSubjectsController < ApplicationController
   def index
-    ids = (cookies["subjects"] || "") .split(',').reject(&:blank?)
-    @subjects = SummarizedSubject.where(id: ids)
+    subject_ids = ids "get"
+    @subjects = SummarizedSubject.where(id: subject_ids)
     @schedules = cookies["subject_schedules"] || {}
   end
   
@@ -22,34 +22,43 @@ class StockSubjectsController < ApplicationController
   def create
     weeks, periods = get_week_and_periods
     begin
-      data = [weeks[params[:week].to_i], periods[params[:period].to_i]]
+      week = params[:week] ? weeks[params[:week].to_i] : nil
+      value = params[:period] ? periods[params[:period].to_i] : nil
       subject_id = params[:subject_id]
     rescue
       data = nil
       subject_id = nil
     end
     respond_to do |format|
-      if data and subject_id
-        schedules "add", [subject_id, data]
+      if week && value && subject_id
+        schedules "add", [subject_id, [week, value]]
         ids "add", subject_id
-        format.js { @status = "success"}
+        format.js { @status = {"status": "success", "id": subject_id} }
       else
-        format.js { @status = "fail"}
+        format.js { @status = {"status": "fail"} }
       end
     end
   end
 
   def destroy
     subject_id = params[:id]
-    schedules "delete", subject_id
-    ids "delete", subject_id
+    respond_to do |format|
+      if subject_id
+        schedules "delete", subject_id
+        ids "delete", subject_id
+        p subject_id
+        format.js { @status = {"status": "success", "id": subject_id} }
+      else
+        format.js { @status = {"status": "fail"} }
+      end
+    end
   end
   
   def get_week_and_periods
     return [["月", "火", "水", "木", "金", "土"], Array(1..7)]
   end
   
-  def schedules(method, data)
+  def schedules(method, data=nil)
     cookie_hash = cookies["subject_schedules"].present? ? JSON.parse(cookies["subject_schedules"]).stringify_keys : {}
     if method == 'add'
       subject_id, value = data[0], data[1]
@@ -59,15 +68,14 @@ class StockSubjectsController < ApplicationController
     elsif method == 'get'
       return cookie_hash
     end
-    cookies["subject_schedules"] = cookie_hash.stringify_keys
+    cookies["subject_schedules"] = JSON.dump cookie_hash.stringify_keys
   end
   
-  def ids(method, data)
+  def ids(method, data=nil)
     subject_ids = (cookies["subjects"] || "").split("&").reject(&:blank?)
     if method == "add"
       subject_ids.push(data)
     elsif method == 'delete'
-      binding.pry
       subject_ids.delete(data)
     elsif method == 'get'
       return subject_ids
