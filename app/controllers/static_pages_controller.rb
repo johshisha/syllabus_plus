902657670439ad2be8902642f1e7595716e46c79
johshisha <johshisha@gmail.com>
2017-03-29 03:15:34 +0900

    Send contact

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,19 @@ class StaticPagesController < ApplicationController
   
   def q_and_a
   end
+  
+  def contact
+    @uuid = cookies.signed["uuid"]
+  end
+  
+  def post_contact
+    content = params[:content]
+    if content.present?
+      flash[:success] = "問い合わせありがとうございました．今後ともよろしくお願いします．"
+    else
+      flash[:danger] = "内容が記入されていません．"
+    end
+    render "contact"
+    # binding.pry
+  end
 end
